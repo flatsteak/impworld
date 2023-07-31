41805f0678e230341301d3de863e8864b88207eb
Max Metral <max.metral@sesamecare.com>
2023-07-30 21:32:41 -0400

    feat(cjs): move from iife to cjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   platform: 'browser',
-  format: 'iife',
+  format: 'cjs',
   tsconfig: 'tsconfig.build.json',
   dts: true,
 });
